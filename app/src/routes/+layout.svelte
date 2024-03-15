dcc4ed676672bad625a22882bf7e63765170c882
rdunk <rupert@rupertdunk.com>
2024-03-15 13:59:55 +0000

    chore: add preview toggle

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import { isPreviewing, VisualEditing } from '@sanity/visual-editing/svelte';
-
+	import { page } from '$app/stores';
 	import LiveMode from '../components/LiveMode.svelte';
 </script>
+
+{#if $isPreviewing}
+	<a href={`/preview/disable?redirect=${$page.url.pathname}`} class="preview-toggle">
+		<span>Preview Enabled</span>
+		<span>Disable Preview</span>
+	</a>
+{/if}
 
 <div class="container">
 	<header class="header">
@@ -108,5 +115,47 @@
 		.footer {
 			margin: var(--space-3) 0;
 		}
+	}
+
+	.preview-toggle {
+		backdrop-filter: blur(12px);
+		border-radius: 0.25rem;
+		bottom: 1rem;
+		box-shadow:
+			0 10px 15px -3px rgba(0, 0, 0, 0.1),
+			0 4px 6px -2px rgba(0, 0, 0, 0.05);
+		color: #1f2937;
+		display: block;
+		font-size: 0.75rem;
+		font-weight: 500;
+		line-height: 1rem;
+		padding-bottom: 0.5rem;
+		padding-left: 0.75rem;
+		padding-right: 0.75rem;
+		padding-top: 0.5rem;
+		position: fixed;
+		right: 1rem;
+		text-align: center;
+		text-decoration: none;
+		z-index: 50;
+	}
+
+	.preview-toggle:hover {
+		background-color: #ef4444;
+		color: #ffffff;
+	}
+
+	.preview-toggle span:first-child {
+		display: block;
+	}
+	.preview-toggle:hover span:first-child {
+		display: none;
+	}
+
+	.preview-toggle span:last-child {
+		display: none;
+	}
+	.preview-toggle:hover span:last-child {
+		display: block;
 	}
 </style>
