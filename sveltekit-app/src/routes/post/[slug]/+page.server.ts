96f223840832725e69f5eec6eadd92718e768ea8
Rupert Dunk <rupert@rupertdunk.com>
2025-10-22 20:00:19 +0100

    fix: destructure params in post route

--- sveltekit-app/src/routes/post/[slug]/+page.server.ts
+++ sveltekit-app/src/routes/post/[slug]/+page.server.ts
@@ -3,14 +3,15 @@ import type {PageServerLoad} from './$types'
 
 export const load: PageServerLoad = async ({locals, params}) => {
   const {loadQuery} = locals.sanity
-  const initial = await loadQuery<Post>(query, params)
+  const {slug} = params
+  const initial = await loadQuery<Post>(query, {slug})
 
   // We pass the data in a format that is easy for `useQuery` to consume in the
   // corresponding `+page.svelte` file, but you can return the data in any
   // format you like.
   return {
     query,
-    params,
+    params: {slug},
     options: {initial},
   }
 }
