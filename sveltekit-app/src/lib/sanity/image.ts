223fa2243ff5667a0f59677788e40c3f2e577634
KenJonesPizza <kenjwho@gmail.com>
2026-01-29 12:42:29 -0600

    upgrade all dependencies.  Update breaking changes for @sanity/image-url.

--- sveltekit-app/src/lib/sanity/image.ts
+++ sveltekit-app/src/lib/sanity/image.ts
@@ -1,8 +1,8 @@
-import imageUrlBuilder from '@sanity/image-url'
+import {createImageUrlBuilder} from '@sanity/image-url'
 import type {Image} from '@sanity/types'
 import {client} from './client'
 
-const builder = imageUrlBuilder(client)
+const builder = createImageUrlBuilder(client)
 
 export function urlFor(source: Image) {
   return builder.image(source)
