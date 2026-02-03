f72302627c4d6992ae8aac0102fa063c364e9a5f
Rupert Dunk <rupert@rupertdunk.com>
2026-02-03 19:29:37 +0000

    fix: resolve autoUpdates deprecation warning

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -16,5 +16,5 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
-  autoUpdates: true,
+  deployment: {autoUpdates: true},
 })
